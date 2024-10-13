f7be7cfa627e9b0950e308ec0f81e38f3b8ed5ad
ADavlatov <a@davlatov.space>
2024-10-14 02:14:51 +0300

    Service for start processes

---
+++ GridSystem.Client/Services/ProcessService.cs
@@ -0,0 +1,11 @@
+﻿namespace GridSystem.Client.Services;
+
+public static class ProcessService
+{
+    public static string Launch(byte[] file)
+    {
+        Thread.Sleep(5000);
+        Random rnd = new Random();
+        return rnd.Next(0, 100000).ToString();
+    }
+}
