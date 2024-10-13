eafd5a96ca54534ee6afca8889ee15d4e05393d2
ADavlatov <a@davlatov.space>
2024-10-14 02:13:35 +0300

    Add context for local db with files

---
+++ GridSystem.Server/Database/AppContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GridSystem.Server.Database;
+
+public class AppContext : DbContext
+{
+    public DbSet<FileModel> Files { get; set; }
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=files.db");
+    }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<FileModel>(entity => 
+        {
+            entity.HasKey(x => x.Id);
+            entity.HasOne(x => x.Name);
+            entity.HasOne(x => x.Bytes);
+        });
+    }
+}
