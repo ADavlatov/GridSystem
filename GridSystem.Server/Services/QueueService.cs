4af0b1f25863242fe41f29397b9d74b172fdc25d
ADavlatov <a@davlatov.space>
2024-10-14 02:15:07 +0300

    Service for sending files from db

---
+++ GridSystem.Server/Services/QueueService.cs
@@ -0,0 +1,22 @@
+﻿using Google.Protobuf;
+using Grpc.Core;
+using Microsoft.EntityFrameworkCore;
+using AppContext = GridSystem.Server.Database.AppContext;
+
+namespace GridSystem.Server.Services;
+
+public class QueueService(AppContext db) : Grid.GridBase
+{
+    public override async Task<FileResponse> SendFile(FileRequest request, ServerCallContext context)
+    {
+        var filesCount = await db.Files.CountAsync();
+        var file = await db.Files.FirstOrDefaultAsync();
+
+        if (file == null)
+        {
+            return new FileResponse { Status = "Empty" };
+        }
+
+        return new FileResponse { File = ByteString.FromBase64(file.Bytes), Status = "Осталось файлов: " + filesCount };
+    }
+}
