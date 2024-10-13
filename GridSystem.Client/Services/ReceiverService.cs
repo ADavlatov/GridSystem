ad53c9988db0ff364519851e73310a27ff917687
ADavlatov <a@davlatov.space>
2024-10-14 02:15:39 +0300

    Service to get files and send results

---
+++ GridSystem.Client/Services/ReceiverService.cs
@@ -0,0 +1,39 @@
+﻿using Grpc.Net.Client;
+
+namespace GridSystem.Client.Services;
+
+public class ReceiverService
+{
+    private readonly Grid.GridClient _gridClient;
+
+    public ReceiverService()
+    {
+        //@TODO вынести host в appsettings
+        var channel = GrpcChannel.ForAddress("");
+        _gridClient = new Grid.GridClient(channel);
+    }
+
+    public async Task Perform()
+    {
+        int state = 0;
+        string result = "";
+
+        while (true)
+        {
+            var response = await GetFile(state, result);
+
+            if (response.Status == "Empty")
+            {
+                break;
+            }
+
+            state = 1;
+            result = ProcessService.Launch(response.File.ToByteArray());
+        }
+    }
+
+    private async Task<FileResponse> GetFile(int state, string result)
+    {
+        return await _gridClient.SendFileAsync(new FileRequest { State = state, Result = result });
+    }
+}
