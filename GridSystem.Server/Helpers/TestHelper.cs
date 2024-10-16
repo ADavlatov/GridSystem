c078d2a377e82d864b5143bbcf2cc4f20169edcd
ADavlatov <a@davlatov.space>
2024-10-16 13:40:13 +0300

    Test files generator

---
+++ GridSystem.Server/Helpers/TestHelper.cs
@@ -0,0 +1,19 @@
+﻿namespace GridSystem.Server.Helpers;
+
+public class TestHelper
+{
+    public void GenerateFiles(int count, string directory)
+    {
+        var random = new Random();
+        string name = "";
+        string path = "";
+        
+        for (int i = 0; i < count; i++)
+        {
+            name = $"test{i}.txt";
+            path = Path.Combine(directory, name);
+            
+            File.WriteAllText(path, random.Next(1000000).ToString());
+        }
+    }
+}
