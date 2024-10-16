0931ffbb60aa2d8ac02c5d504b735f72a178c96d
ADavlatov <a@davlatov.space>
2024-10-16 13:38:30 +0300

    Конвертация файлов из директории в массив байтов и запись их в бд

--- GridSystem.Server/Services/FilesService.cs
+++ GridSystem.Server/Services/FilesService.cs
@@ -1,9 +1,21 @@
-﻿namespace GridSystem.Server.Services;
+﻿using GridSystem.Server.Database;
+using AppContext = GridSystem.Server.Database.AppContext;
 
-public class FilesService
+namespace GridSystem.Server.Services;
+
+public class FilesService(AppContext db)
 {
     public async Task ConvertFilesToBase64AndSave(string filePath)
     {
+        string[] files = Directory.GetFiles(filePath, "*.txt");
         
+        foreach (string file in files)
+        {
+            // Чтение содержимого файла в массив байтов
+            byte[] byteArray = File.ReadAllBytes(file);
+            db.Files.Add(new FileModel { Name = file, Bytes = byteArray });
+        }
+
+        db.SaveChanges();
     }
 }
