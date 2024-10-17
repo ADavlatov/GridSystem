81b354d44d56cafa8ae9c09f38344e2ce623479b
ADavlatov <a@davlatov.space>
2024-10-17 11:01:04 +0300

    Add polling service

---
+++ GridSystem.Server/Services/PollingService.cs
@@ -0,0 +1,14 @@
+namespace GridSystem.Server.Services;
+
+public class PollingService(IHttpClientFactory httpClientFactory) : IHostedService
+{
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
