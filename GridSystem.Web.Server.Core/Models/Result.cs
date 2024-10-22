a65c7ecb9813ae4728a8c94407c79b1c3b3f483c
ADavlatov <a@davlatov.space>
2024-10-23 00:29:16 +0300

    Models for core service

---
+++ GridSystem.Web.Server.Core/Models/Result.cs
@@ -0,0 +1,8 @@
+﻿namespace GridSystem.Web.Server.Core.Models;
+
+public record Result
+{
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public string Value { get; set; }
+}
---
+++ GridSystem.Web.Server.Core/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace GridSystem.Web.Server.Core.Models;
+
+public record User
+{
+    public Guid Id { get; set; }
+    public string Status { get; set; }
+    public string Key { get; set; }
+    public List<Result> Results { get; set; }
+}
