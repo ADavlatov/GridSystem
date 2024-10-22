22bb3d7be4856b1c65c8d50b74b145606743bdbc
ADavlatov <a@davlatov.space>
2024-10-23 00:28:26 +0300

    Add polling to check status

--- GridSystem.Server/Services/PollingService.cs
+++ GridSystem.Server/Services/PollingService.cs
@@ -2,13 +2,23 @@ namespace GridSystem.Server.Services;
 
 public class PollingService(IHttpClientFactory httpClientFactory) : IHostedService
 {
-    public Task StartAsync(CancellationToken cancellationToken)
+    private bool _isPollingWork = true;
+    public async Task StartAsync(CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        var httpClient = httpClientFactory.CreateClient();
+        while (_isPollingWork)
+        {
+            Thread.Sleep(1000);
+            var response = await httpClient.GetAsync("https://localhost:7213/api/v1/core/status");
+        
+            Console.WriteLine(response.StatusCode);
+        }
     }
 
     public Task StopAsync(CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        _isPollingWork = false;
+        
+        return Task.CompletedTask;
     }
 }
