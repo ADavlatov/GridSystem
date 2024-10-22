09d7c61bf6517685db3dfb17d96bf9bf9d218e96
ADavlatov <a@davlatov.space>
2024-10-23 00:29:07 +0300

    Add hosted service

--- GridSystem.Server/Program.cs
+++ GridSystem.Server/Program.cs
@@ -1,11 +1,13 @@
+using System.Net;
 using GridSystem.Server.Services;
 using AppContext = GridSystem.Server.Database.AppContext;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddHttpClient();
 
+builder.Services.AddHttpClient();
 builder.Services.AddDbContext<AppContext>();
 builder.Services.AddGrpc();
+builder.Services.AddHostedService<PollingService>();
 
 var app = builder.Build();
 
@@ -13,5 +15,3 @@ app.MapGet("/", () => { Console.WriteLine("Hello 2");});
 
 app.MapGrpcService<QueueService>();
 app.Run();
-
-Console.ReadKey();
