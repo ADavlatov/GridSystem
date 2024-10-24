075960586f0709b11707d90d0a08a3add78a0dfd
ADavlatov <a@davlatov.space>
2024-10-25 00:45:47 +0300

    Fix context

--- GridSystem.Web.Server.Core/Contexts/CoreContext.cs
+++ GridSystem.Web.Server.Core/Contexts/CoreContext.cs
@@ -5,35 +5,32 @@ namespace GridSystem.Web.Server.Core.Contexts;
 
 public class CoreContext : DbContext
 {
-    public class UserContext : DbContext
+    public DbSet<User> Users { get; set; }
+    public DbSet<Result> Results { get; set; }
+
+    public CoreContext()
     {
-        public DbSet<User> Users { get; set; }
-        public DbSet<Result> Results { get; set; }
-
-        public UserContext()
-        {
-            Database.EnsureCreated();
-        }
-
-        //@TODO заменить на постгресс, для тестов используется sqlite
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlite("Data Source=Users.db");
-        }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<User>(entity => 
-            {
-                entity.HasKey(x => x.Id);
-                entity.HasMany(x => x.Results).WithOne(x => x.User);
-            });
-
-            modelBuilder.Entity<Result>(entity =>
-            {
-                entity.HasKey(x => x.Id);
-                entity.HasOne(x => x.User).WithMany(x => x.Results);
-            });
-        }
+        Database.EnsureCreated();
     }
- }
+
+    //@TODO заменить на постгресс, для тестов используется sqlite
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=Users.db");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>(entity => 
+        {
+            entity.HasKey(x => x.Id);
+            entity.HasMany(x => x.Results).WithOne(x => x.User);
+        });
+
+        modelBuilder.Entity<Result>(entity =>
+        {
+            entity.HasKey(x => x.Id);
+            entity.HasOne(x => x.User).WithMany(x => x.Results);
+        });
+    }
+}
