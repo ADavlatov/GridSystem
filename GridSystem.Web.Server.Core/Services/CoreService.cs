67f6f797d878b8e0889f7d2199e48de211988fd9
ADavlatov <a@davlatov.space>
2024-10-25 00:46:29 +0300

    Add methods to core service

--- GridSystem.Web.Server.Core/Services/CoreService.cs
+++ GridSystem.Web.Server.Core/Services/CoreService.cs
@@ -1,17 +1,18 @@
+using GridSystem.Web.Server.Core.Contexts;
 using Grpc.Core;
 
 namespace GridSystem.Web.Server.Core.Services;
 
-public class CoreService : Core.CoreBase
+public class CoreService(CoreContext db) : Core.CoreBase
 {
     public override async Task<SetStatusResponse> SetStatus(SetStatusRequest request, ServerCallContext context)
     {
-        return new SetStatusResponse { IsSucceed = true, Status = 200, Error = "" };
+        return await new UserService(db).SetStatus(request);
     }
 
-    public override async Task<GetAttributesResponse> GetUserAttributes(GetAttributesRequest request,
+    public override async Task<GetStatusResponse> GetStatus(GetStatusRequest request,
         ServerCallContext context)
     {
-        return new GetAttributesResponse { IsSucceed = true, Status = 200, Error = "", Key = "", UserStatus = 0 };
+        return await new UserService(db).GetStatus(request);
     }
 }
