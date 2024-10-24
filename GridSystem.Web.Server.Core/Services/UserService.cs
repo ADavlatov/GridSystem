fb0aca52866ccb1a2f51dde6f0bdf7c1c5eaa8c4
ADavlatov <a@davlatov.space>
2024-10-25 00:46:19 +0300

    Fix user service

--- GridSystem.Web.Server.Core/Services/UserService.cs
+++ GridSystem.Web.Server.Core/Services/UserService.cs
@@ -1,6 +1,4 @@
-using System.IdentityModel.Tokens.Jwt;
 using GridSystem.Web.Server.Core.Contexts;
-using GridSystem.Web.Server.Core.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace GridSystem.Web.Server.Core.Services;
@@ -10,24 +8,30 @@ public class UserService(CoreContext db)
     public async Task<SetStatusResponse> SetStatus(SetStatusRequest request)
     {
         var userId = Guid.Parse(request.UserId);
-        var key = Guid.Parse(request.Key);
+        var user = await db.Users.FirstOrDefaultAsync(x => x.Id == userId);
 
-        var user = await db.Attributes.FirstAsync(x => x.UserId == id);
-
-        if (user.Key != Guid.Parse(securityKey))
+        if (user == null)
         {
-            return new SetStatusResponse { IsSucceed = false, Status = 400, Error = "Wrong security token" };
+            return new SetStatusResponse { IsSucceed = false, Status = 400, Error = "User not found." };
         }
-
+        
         user.Status = request.Status;
-        db.Attributes.Update(user);
+        db.Users.Update(user);
         await db.SaveChangesAsync();
-
-        return new SetStatusResponse { IsSucceed = true, Status = 400, Error = "" };
+        
+        return new SetStatusResponse { IsSucceed = true, Status = 200, Error = "" };
     }
 
-    public async Task<GetAttributesResponse> GetAttributes(GetAttributesRequest request)
+    public async Task<GetStatusResponse> GetStatus(GetStatusRequest request)
     {
-        
+        var userId = Guid.Parse(request.UserId);
+        var user = await db.Users.FirstOrDefaultAsync(x => x.Id == userId);
+
+        if (user == null)
+        {
+            return new GetStatusResponse { IsSucceed = false, Status = 400, Error = "User not found." };
+        }
+
+        return new GetStatusResponse { IsSucceed = true, Status = 200, Error = "", UserStatus = user.Status };
     }
 }
